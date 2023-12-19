dee1369c72d293b63e8f665447eea6700da2ec48
Eduard Karpiel <karpiel@shoptet.cz>
2023-12-19 17:49:33 +0100

    2022 Day 14

---
+++ 2022/14/part2.php
@@ -0,0 +1,38 @@
+<?php
+
+function place_sand(int $x, int $y, int $maxY, array &$map, int &$count): bool
+{
+    if ($y >= $maxY + 2 || isset($map[$x][$y])) {
+        return false;
+    }
+    if (!place_sand($x, $y + 1, $maxY, $map, $count)
+        && !place_sand($x - 1, $y + 1, $maxY, $map, $count)
+        && !place_sand($x + 1, $y + 1, $maxY, $map, $count)) {
+        $map[$x][$y] = 'o';
+        ++$count;
+    }
+    return true;
+}
+
+$lines = file('input.txt', FILE_IGNORE_NEW_LINES);
+$map = [];
+$maxY = 0;
+foreach ($lines as $line) {
+    preg_match_all('/(\d+),(\d+)/', $line, $matches, PREG_SET_ORDER);
+    for ($i = 0; $i < count($matches) - 1; ++$i) {
+        foreach (range($matches[$i][1], $matches[$i + 1][1]) as $x) {
+            foreach (range($matches[$i][2], $matches[$i + 1][2]) as $y) {
+                $map[$x][$y] = 1;
+                $maxY = max($maxY, $y);
+            }
+        }
+    }
+}
+
+$prevCount = $count = 0;
+do {
+    $prevCount = $count;
+    place_sand(500, 0, $maxY, $map, $count);
+} while ($count !== $prevCount);
+
+var_dump($count);
